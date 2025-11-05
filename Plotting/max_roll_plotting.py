eb35f494a39cdd34607ac42a2c0db3809f1ed4c9
max-hug <maxhug@u.northwestern.edu>
2025-11-05 16:38:13 -0600

    pure simulink model

--- Plotting/max_roll_plotting.py
+++ Plotting/max_roll_plotting.py
@@ -5,7 +5,7 @@ from scipy.signal import savgol_filter
 from scipy.fft import fft, fftfreq, rfft, rfftfreq
 
 # --- Load CSV data ---
-df = pd.read_csv('C:\\Users\\maxhu\\Documents\\VS_Code\\Roll-Control\\Plotting\\FT1_primary.csv')
+df = pd.read_csv('Plotting\\Data\\FT1_primary.csv')
 df = df.set_index('time')
 df = df[~df.index.duplicated(keep='first')]
 
@@ -17,6 +17,7 @@ df = df.reset_index()
 
 # --- Basic plot ---
 df_main = df[df['time'] > 70]
+df_main = df_main[df_main['time'] < 80]
 
 time = df_main['time']
 roll = df_main['gyro_roll']
@@ -24,7 +25,7 @@ roll = df_main['gyro_roll']
 print(time)
 
 roll_smooth = savgol_filter(roll, window_length=20, polyorder=3)
-roll_dot = savgol_filter(roll, window_length=20, polyorder=3, deriv=1, delta=dt)
+roll_dot = savgol_filter(roll, window_length=21, polyorder=3, deriv=1, delta=dt)
 
 print(max(roll_dot)*(np.pi/180)*1)
 
@@ -35,7 +36,7 @@ fft_magnitude = np.abs(fft_vals) / len(roll_dot) * 2  # scale amplitude
 
 
 # Make subplots: 2 rows, 1 column
-fig, ax = plt.subplots(3, 1, figsize=(8,6), sharex=True)
+fig, ax = plt.subplots(3, 1, figsize=(8,6))
 
 # Plot roll
 ax[0].plot(time, roll_smooth, label='Smooth Roll')
@@ -52,6 +53,9 @@ ax[1].legend()
 ax[1].grid(True)
 
 ax[2].plot(fft_freqs, fft_magnitude)
+ax[2].set_xlim([0,5])
+ax[2].set_xlabel('Frequnecy [Hz]')
+ax[2].set_ylabel('Magnitude')
 
 #Tight layout for spacing
 plt.tight_layout()
